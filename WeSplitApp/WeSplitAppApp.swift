4667664ccc28a5edc2c7f2ebb3b8312a75953985
kiran sonne <kiransonne04@gmail.com>
2022-12-24 21:53:01 +0530

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Kiran Sonne on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
